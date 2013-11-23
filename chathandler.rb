5e4bfbf20ef43f21e192958376d3f7e13f629a71
pickdenis <pickmydenis>
2013-11-23 12:40:11 -0700

    First commit

---
+++ chathandler.rb
@@ -0,0 +1,69 @@
+module ChatHandler
+  
+  TRIGGERS = []
+  
+  def self.make_info message, ws
+    info = {
+      room: message[0][1..-2],
+      where: "c",
+      who: message[2][1..-1],
+      what: message[3],
+      ws: ws
+    }
+    
+    if message[1] == "pm"
+      info[:where] = "pm"
+      info[:what] = message[4]
+      info[:to] = message[3][1..-1]
+    end
+    
+    info
+  end
+  
+  
+  def self.handle message, ws
+    m_info = make_info(message, ws)
+    TRIGGERS.each do |t|
+      result = t.is_match?(m_info)
+      
+      if result
+        m_info[:result] = result
+        m_info[:respond] = if m_info[:where] == 'c'
+          proc { |mtext| m_info[:ws].send("#{m_info[:room]}|#{mtext}") }
+        else 
+          proc { |mtext| m_info[:ws].send("|/pm #{m_info[:who]},#{mtext}") } 
+        end 
+        t.do_act(m_info)
+      end
+      
+    end
+  end
+
+end
+
+class Trigger
+  
+  def initialize &blk
+    yield self
+  end
+  
+  def match &blk
+    @match = blk
+  end
+  
+  def act &blk
+    @action = blk
+  end
+  
+  def is_match? m_info
+    @match.call(m_info)
+  end
+  
+  def do_act m_info
+    @action.call(m_info)
+  end
+end
+
+# require all trigger files here
+
+require './statcalc_trigger.rb'
---
+++ statcalc_trigger.rb
@@ -0,0 +1,94 @@
+
+ChatHandler::TRIGGERS << Trigger.new do |t|
+  t.match { |info|
+    info[:what][0..3] == 'base' &&
+    sc = info[:what].scan(/[:\+\-\w]+/) and sc }
+  
+  t.act do |info|
+    # initialize all variables
+    base = 0
+    iv = 31.0
+    ev = 0.0
+    level = 100.0
+    modifier = 1.0
+    boostmod = 1.0
+    hp = false
+    as_base = as_uninvested_base = false
+    naturemod = 1.1
+    plus = minus = 0.0
+    
+    stat = 0.0
+    
+    # loop through the list of flags
+    info[:result].each do |term|
+      case term
+      when /base:(\d+)/
+        base = $1.to_f
+      when /level:(\d+)/
+        level = $1.to_f
+      when /(?:ev|evs):(\d+)/
+        ev = $1.to_f
+      when /(?:iv|ivs):(\d+)/
+        iv = $1.to_f
+      when /\+(\d+)/
+        plus = $1.to_f
+        boostmod = (plus + 2) / 2
+      when /\-(\d+)/
+        minus = $1.to_f
+        boostmod = 2 / (minus + 2)
+      when "doubled"
+        modifier *= 2
+      when /scarf(ed)?/
+        modifier *= 1.5
+      when "invested"
+        ev = 252
+      when "asbase"
+        as_base = true
+      when "uninvested"
+        as_uninvested_base = true
+      when /(bad)(nature)?/
+        naturemod = 1.0
+      when "hp"
+        hp = true
+      end
+    end
+    
+    # calculate the stats
+    stat = !hp ? ((((iv + 2*base + ev/4) * level/100.0 + 5) * modifier * boostmod).to_i * naturemod).to_i
+               : (iv + 2*base + ev/4) * level/100.0 + 10 + level
+        
+    if as_base
+      if as_uninvested_base
+        stat = (((stat.to_f/naturemod - 5) * 100.0/level - iv) / 2).to_i
+      else
+        p stat
+        stat = (((stat.to_f/naturemod - 5) * 100.0/level - ev/4 - iv) / 2).to_i
+      end
+      
+    end
+    
+    # format and send the message
+    
+    result = "#{base.to_i} base stat #{ev == 252 ? 'invested' : ev > 0 ? "at #{ev} evs" : 'uninvested'} "
+    
+    result << if plus == 0
+      if minus == 0
+        ""
+      else
+        "at -#{minus.to_i} "
+      end
+    else
+      "at +#{plus.to_i} "
+    end
+    
+    result << " #{modifier > 1 ? "with a modifier of #{modifier}x" : ''} "
+    
+    result << if as_base
+      "is equivalent to a base stat of #{stat} #{as_uninvested_base ? 'without investment' : 'with same investment'}."
+    else
+      "results in a stat of #{stat}."
+    end
+    
+    info[:respond].call("#{result}")
+  end
+end
