fae8f54e4081775cf84280e00beff73690525e74
pickdenis <pickmydenis>
2013-11-28 10:11:50 -0700

    Add FC trigger, among other fixes

--- bread/bread_trigger.rb
+++ bread/bread_trigger.rb
@@ -10,18 +10,17 @@ ChatHandler::TRIGGERS << Trigger.new do |t| # breadfinder
   }
   
   t.act do |info|
-    if t[:lastused] + t[:cooldown] < Time.now
+    t[:lastused] + t[:cooldown] < Time.now or return
     
-      t[:lastused] = Time.now
-      
-      bread = BreadFinder.get_bread
-      result = if bread[:no] == 0
-        "couldn't find the bread, sorry"
-      else
-        "bread: http://4chan.org/vp/res/#{bread[:no]}#bottom"
-      end
-      info[:respond].call(result)
+    t[:lastused] = Time.now
+    
+    bread = BreadFinder.get_bread
+    result = if bread[:no] == 0
+      "couldn't find the bread, sorry"
+    else
+      "bread: http://4chan.org/vp/res/#{bread[:no]}#bottom"
     end
+    info[:respond].call(result)
   end
 end << Trigger.new do |t| # battles
   t[:lastused] = Time.now
@@ -32,19 +31,18 @@ end << Trigger.new do |t| # battles
   }
   
   t.act do |info|
-    if t[:lastused] + t[:cooldown] < Time.now
+    t[:lastused] + t[:cooldown] < Time.now or return
     
-      t[:lastused] = Time.now
-      
-      battles = Battles.get_battles
-      
-      result = if battles == []
-        "couldn't find any battles, sorry"
-      else
-        "champ battle: #{battles.last}"
-      end
-      
-      info[:respond].call(result)
+    t[:lastused] = Time.now
+    
+    battles = Battles.get_battles
+    
+    result = if battles == []
+      "couldn't find any battles, sorry"
+    else
+      "champ battle: #{battles.last}"
     end
+    
+    info[:respond].call(result)
   end
 end
