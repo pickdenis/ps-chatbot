84dbf4a31a89445084da5e42dd7af78e68c8ea11
pickdenis <pickmydenis>
2013-11-28 14:57:49 -0700

    Add battle reporter

---
+++ bread/battle_reporter_trigger.rb
@@ -0,0 +1,24 @@
+require "./bread/battles.rb"
+
+$chat << Trigger.new do |t|
+  t[:lastused] = Time.now
+  t[:cooldown] = 10
+  t[:lastbattle] = nil
+  
+  t.match { |info| 
+    info[:where] == 'c'
+  }
+  
+  t.act do |info|
+    t[:lastused] + t[:cooldown] < Time.now or next
+    
+    puts "Checking for a new battle"
+    lastbattle = Battles.get_battles.last
+    
+    if t[:lastbattle] != lastbattle
+      t[:lastbattle] = lastbattle
+      info[:respond].call(lastbattle)
+    end
+    
+  end
+end
