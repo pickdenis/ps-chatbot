dfbca6ab44187937921e34370334a7a6d1db42cd
pickdenis <pickmydenis>
2013-12-03 06:55:12 -0700

    Add cooldown to stacalc

--- showderp/statcalc/statcalc_trigger.rb
+++ showderp/statcalc/statcalc_trigger.rb
@@ -1,6 +1,8 @@
 
 $chat << Trigger.new do |t|
   t[:id] = 'statcalc'
+  t[:cooldown] = 2 # seconds
+  t[:lastused] = Time.now - t[:cooldown]
   
   t.match { |info|
     info[:what][0..4] == 'base:' &&
@@ -8,6 +10,10 @@ $chat << Trigger.new do |t|
   }
   
   t.act do |info|
+    t[:lastused] + t[:cooldown] < Time.now or next
+
+    t[:lastused] = Time.now
+    
     # initialize all variables
     base = 0
     iv = 31.0
