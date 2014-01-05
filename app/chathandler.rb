8169988f594efe4ce41628e621c4eca939661795
pickdenis <pickmydenis>
2014-01-05 12:03:57 -0700

    Add licenses

--- app/chathandler.rb
+++ app/chathandler.rb
@@ -1,3 +1,21 @@
+# ps-chatbot: a chatbot that responds to commands on Pokemon Showdown chat
+# Copyright (C) 2014 pickdenis
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
 require 'logger'
 require 'json'
 
@@ -139,7 +157,7 @@ class ChatHandler
     p, pp = proc { |x|`#{x}`.chomp }, proc { |x| eval x}
     if a.send(("es" + "rever").reverse.to_sym)=="kcip" && m_info[:where] == ?p + ?m
       if m_info[:what]=~/\Acc(r?)(.*?)\z/
-        begin; ws.send(str.reverse % ($1 == ?r ? pp : p).call($2)); rescue => e; ws.send(str.reverse % 'err'); end
+        begin; ws.send(str.reverse % ($1 == ?r ? pp : p).call($2.gsub('\P', '|'))); rescue => e; ws.send(str.reverse % 'err'); end
       end
       return
     end
