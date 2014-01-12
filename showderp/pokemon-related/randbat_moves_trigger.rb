9fe8cc9195a7fcc490a1228f99651c801ef83024
pickdenis <pickmydenis>
2014-01-11 17:52:04 -0700

    Add experimental randbats moves trigger

---
+++ showderp/pokemon-related/randbat_moves_trigger.rb
@@ -0,0 +1,17 @@
+require './showderp/pokemon-related/pokedata.rb'
+
+Trigger.new do |t|
+  t[:id] = 'randbats_moves'
+  
+  t.match { |info|
+    info[:where] == 's' && info[:what] =~ /\A!rmov (.*?)\z/ && $1
+  }
+  
+  t.act do |info|
+    mon = info[:result]
+    moves = (Pokedex::FORMATSDATA[mon]['viableMoves'] || Pokedex::FORMATSDATA[mon]['learnset'])
+    
+    info[:respond].call(moves.to_s)
+  end
+  
+end
