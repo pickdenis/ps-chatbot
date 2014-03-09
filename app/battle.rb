4bbbe99da9b6c92417e358e4c052358ce97ee8c6
pickdenis <pickmydenis>
2014-03-09 15:37:24 -0700

    rudimentary cc1v1 battling (auto-accept challenge)

--- app/battle.rb
+++ app/battle.rb
@@ -33,8 +33,8 @@ class BattleHandler
   end
   
   def handle_challenge message, ws
-    JSON.parse(message[2])["challengesFrom"].each do |who, data|
-      if data["format"] == "challengecup1vs1"
+    JSON.parse(message[2])["challengesFrom"].each do |who, format|
+      if format == "challengecup1vs1"
         ws.send("|/utm {}")
         ws.send("|/accept #{who}, {}")
       end
@@ -42,14 +42,30 @@ class BattleHandler
     
   end
   
+  def self.parse_battle_id id
+    parts = id.split('-')
+    
+    # battle-format-number
+    parts.shift # throw away the first prat
+    
+    { format: parts.shift, number: parts.shift}
+  end
+  
 end
 
 class BattleAdapter
-  attr_accessor :battle, :respond, :id
+  attr_accessor :battle, :respond, :id, :rqid
   
   def initialize args
     @id = args[:id]
-    @battle = Battle.new id: @id # Battle object
+    format = BattleHandler.parse_battle_id(@id)[:format]
+    
+    # If the format is not recognized, it will try to use the default logic, which throws
+    # an exception with every method. Only accept battles that have logic
+    logic = ({'challengecup1vs1' => CC1vs1Logic}[format] || BattleLogic).new
+    
+    
+    @battle = Battle.new id: @id, logic: logic # Battle object
     @respond = args[:respond] # This should be passed from the ChatHandler
   end
   
@@ -62,10 +78,41 @@ class BattleAdapter
     when 'poke'
       # for now, this stuff doesn't matter
     when 'request'
-      data = JSON.parse(message[1])["side"]
-      player = data["id"]
-      # battle
-      @battle.send(player)
+      request = JSON.parse(message[1])
+      @rqid = request['rqid']
+      
+      sidedata = request['side']
+      player = sidedata['id']
+      
+      # let the battle know who it's controlling
+      @battle.set_me(player)
+      
+      # set the data
+      p_object = @battle.send(player)
+      p_object.name = sidedata[:name]
+      p_object.team = {}
+      
+      p_object.side = []
+      sidedata['pokemon'].each_with_index do |poke, index|
+        poke_object = Pokemon.new(
+            ident: poke['ident'], details: poke['details'],condition: poke['condition'], active: poke['active'],
+            stats: poke['stats'], moves: poke['moves'],base_ability: poke['baseability'], item: poke['item'], can_mega_evo: poke['canmegaevo'])
+        
+        p_object.team[poke['ident']] = poke_object
+        
+        p_object.side << if request['active']
+          
+          {object: poke_object, moves: request['active'][index]['moves']}
+        else
+          {object: poke_object}
+        end
+        
+      end
+      
+    when 'teampreview'
+      respond(@battle.logic.chooselead(@rqid))
+    when 'turn'
+      respond(@battle.logic.move(@rqid))
     when 'win'
       respond('/leave')
     end
@@ -73,7 +120,7 @@ class BattleAdapter
 end
 
 class Battle # Hold the logical state of the battle
-  attr_accessor :id, :p1, :p2
+  attr_accessor :id, :p1, :p2, :logic
   
   def initialize args
     @id = args[:id]
@@ -83,27 +130,32 @@ class Battle # Hold the logical state of the battle
     @p1 = Player.new
     @p2 = Player.new
     
+    @logic = args[:logic]
+    
+    
   end
   
   def add_to_team player, poke
     player.team << poke
   end
   
-  def move
-    
-  end
-  
-  def switch
-    
-  end
-  
-  def status
-    
+  def set_me id
+    if id == 'p1'
+      @logic.me = @p1
+      @logic.other = @p2
+    else
+      @logic.me = @p2
+      @logic.other = @p1
+    end
   end
 end
 
 class Player
-  attr_accessor :name, :team, :active_poke
+  attr_accessor :name, :team, :side
+  
+  def initialize *argv
+    @name, @team = argv
+  end
 end
 
 class Pokemon
@@ -114,7 +166,7 @@ class Pokemon
     @details = args[:details]
     @condition = args[:condition]
     @active = args[:active]
-    @bsae_ability = args[:base_ability]
+    @base_ability = args[:base_ability]
     @moves = args[:moves]
     @item = args[:item]
     @can_mega_evo = args[:can_mega_evo]
@@ -122,7 +174,40 @@ class Pokemon
 end
 
 class BattleLogic
-  def turn
+  attr_accessor :me, :other
+  
+  def initialize *argv
+    @me, @other = argv
+  end
+  
+  def move rqid
+    throw NotImplementedError
+  end
+  
+  def chooselead rqid
+    throw NotImplementedError
+  end
+  
+  def switch rqid
+    throw NotImplementedError
+  end
+end
+
+class CC1vs1Logic < BattleLogic
+  def chooselead rqid=
     
+    # Randomly choose a lead
+    lead = rand(1..6)
+    rest = (1..6).to_a
+    rest.unshift(rest.delete(lead))
+    
+    "/team #{rest.join('')}|#{rqid}"
+    
+  end
+  
+  def move rqid
+    
+    chosen = @me.side.first[:moves].select { |move| !move['disabled'] }.sample['move']
+    "/choose move #{chosen}|#{rqid}"
   end
 end
