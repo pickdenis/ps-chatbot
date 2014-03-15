b0bcb03b472710c18db0a49cee3c52d1a6f3428d
pickdenis <pickmydenis>
2014-03-15 16:20:58 -0700

    Mold breaker, moves to the blacklist

--- app/battleutil/cc1vs1helper.rb
+++ app/battleutil/cc1vs1helper.rb
@@ -19,6 +19,12 @@ module CC1vs1
     explosion
     selfdestruct
     
+    synchronoise
+    
+    belch
+    
+    trumpcard
+    wringout
   }
   
   # Can be used, but their scores will be halved
@@ -86,36 +92,48 @@ module CC1vs1
     score *= otherteam_h.map { |poke| (TypeChart.effectiveness(move_h['type'], poke['types']) - 1)/2.0 + 1 }.reduce(:*)
     
     # And finally, abilities
+    my_abilities = species_h['abilities'].values
+    
+    if (my_abilities.index('Huge Power') || my_abilities.index('Pure Power')) && move_h['category'] == 'Physical'
+      # Note: This is an APPROXIMATION. Because this calculation uses the BASE stat instead of the 
+      # calculated stat, this won't be accurate because pure/huge power doesn't double the base stat.
+      # For all of the pokemon it affects, it usually almost triples the effective base stat.
+      score *= 3
+      
+    end
     
     otherteam_h.each do |poke|
       abilities = poke['abilities'].values
       
-      if abilities.index('Levitate')
-        score = 0 if move_h['type'] == 'Ground'
-      end
+      if !my_abilities.index('Mold Breaker')
       
-      if abilities.index('Flash Fire')
-        score = 0 if move_h['type'] == 'Fire'
-      end
-      
-      if abilities.index('Water Absorb') || abilities.index('Storm Drain') || abilities.index('Dry Skin')
-        score = 0 if move_h['type'] == 'Water'
-      end
-      
-      if abilities.index('Volt Absorb') || abilities.index('Motor Drive') || abilities.index('Lightningrod')
-        score = 0 if move_h['type'] == 'Electric'
-      end
-      
-      if abilities.index('Sap Sipper')
-        score = 0 if move_h['type'] == 'Grass'
-      end
-      
-      if abilities.index('Soundproof')
-        score = 0 if ['Hyper Voice', 'Uproar'].index(move_h['name'])
-      end
-      
-      if abilities.index('Wonder Guard')
-        score = 0 if TypeChart.effectiveness(move_h['type'], poke['types']) <= 1
+        if abilities.index('Levitate')
+          score = 0 if move_h['type'] == 'Ground'
+        end
+        
+        if abilities.index('Flash Fire')
+          score = 0 if move_h['type'] == 'Fire'
+        end
+        
+        if abilities.index('Water Absorb') || abilities.index('Storm Drain') || abilities.index('Dry Skin')
+          score = 0 if move_h['type'] == 'Water'
+        end
+        
+        if abilities.index('Volt Absorb') || abilities.index('Motor Drive') || abilities.index('Lightningrod')
+          score = 0 if move_h['type'] == 'Electric'
+        end
+        
+        if abilities.index('Sap Sipper')
+          score = 0 if move_h['type'] == 'Grass'
+        end
+        
+        if abilities.index('Soundproof')
+          score = 0 if ['Hyper Voice', 'Uproar', 'Boomburst', 'Bug Buzz', 'Chatter', 'Relic Sound', 'Echoed Voice'].index(move_h['name'])
+        end
+        
+        if abilities.index('Wonder Guard')
+          score = 0 if TypeChart.effectiveness(move_h['type'], poke['types']) <= 1
+        end
       end
       
     end
