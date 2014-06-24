b6d0514f2c7b9f45fd8818500a1cbcfe0f903c9d
pickdenis <pickmydenis>
2014-06-24 07:14:52 -0700

    !toggle <trigger> to toggle its on/off

---
+++ essentials/trigger_managing/toggle_trigger.rb
@@ -0,0 +1,17 @@
+Trigger.new do |t|
+  t[:id] = 'toggletrigger'
+  
+  t.match { |info|
+    ch.has_access(info[:who]) && info[:what] =~ /\A!toggle (.*?)\z/ && $1
+  }
+  
+  t.act do |info|
+    id = info[:result]
+    t = ch.get_by_id(id)
+    
+    t[:off] = !t[:off]
+    
+    info[:respond].call("Trigger #{t} turned #{t[:off] ? 'off' : 'on'}.")
+    
+  end
+end
